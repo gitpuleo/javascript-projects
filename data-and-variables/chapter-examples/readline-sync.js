b3460057c2ca3ce9cb6ba66139a498116e10ae90
nfp <154305665+gitpuleo@users.noreply.github.com>
2024-06-20 00:18:05 -0500

    completed exercises and concept checks

--- data-and-variables/chapter-examples/readline-sync.js
+++ data-and-variables/chapter-examples/readline-sync.js
@@ -1,3 +1,8 @@
 const input = require('readline-sync');
 
-let info = input.question("Question text... ");
+let name = input.question("Enter your name here: ");
+console.log("Hello, " + name + "!");
+//last nmae next line
+let lastName = input.question("Your last Name? ");
+console.log("Hello there, " + name + " " + lastName + " its nice to meet you!");
+
